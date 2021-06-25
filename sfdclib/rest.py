89d93247971309ec65123025d182c9471ddd614c
Alexander J. Rabey <46423113+ajrabey@users.noreply.github.com>
2021-06-25 13:19:44 -0700

    Removing version

--- sfdclib/rest.py
+++ sfdclib/rest.py
@@ -10,7 +10,7 @@ class SfdcRestApi:
     """ Class to work with Salesforce REST API """
     _API_BASE_URI = "/services/data/v{version}"
     _SOQL_QUERY_URI = "/query/?{query}"
-    _API_APEXREST_URI = "/services/apexrest/v{version}"
+    _API_APEXREST_URI = "/services/apexrest"
 
     def __init__(self, session):
         if not session.is_connected():
@@ -18,8 +18,8 @@ class SfdcRestApi:
         self._session = session
 
     def _get_apexrest_api_uri(self):
-        """Returns APEXREST API base URI for this connection"""
-        return self._API_APEXREST_URI.format(**{'version': self._session.get_api_version()})
+        """Returns APEXREST API base URI without the version for this connection"""
+        return self._API_APEXREST_URI
 
     def _get_api_uri(self):
         """ Returns REST API base URI for this connection """
